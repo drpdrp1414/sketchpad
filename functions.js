31dee3f97d56e644d291f6092ced840ce15991f5
Daniel Pies <drpdrp1414@yahoo.com>
2019-11-12 21:44:02 -0800

    added reset button and new grid create button

--- functions.js
+++ functions.js
@@ -1,6 +1,7 @@
 function genDivs(size)
 {
     var box = document.querySelector(".container")
+    box.innerHTML="";
     for(var i = 0; i < size; i++)
     {
         var row = document.createElement("div")
@@ -9,13 +10,36 @@ function genDivs(size)
         {
             let cell = document.createElement("div")
             cell.className= "gridsquare"
-            //cell.innerText = (i * size) + x
+            //cell.innerText= (i * size) + x
             row.appendChild(cell)
         }
         box.appendChild(row)
     }
-   changeColor()
-       
+    sizeControl()
+    changeColor()   
+}
+
+function newGrid()
+{
+    var size = prompt("Please enter the size of grid you want: ", "16")
+    if(isNaN(size))
+    {
+        alert("Please enter a real number!");
+    }
+    else
+    {
+        genDivs(size)
+    }
+}
+
+function sizeControl(){
+    var cells = document.querySelectorAll(".gridsquare")
+    var cell = document.querySelector(".gridsquare")
+    var dimension = Math.sqrt(cells.length)
+    var heightNew = 660/dimension + "px"
+    
+    /* cells.style.height = heightNew
+    cells.style.width = heightNew */
 }
 
 function changeColor()
@@ -28,18 +52,12 @@ function changeColor()
             cells.item(i).classList.add("permaHover")
         })
     }
-
-    console.log(cells.length)
+    //console.log(cells.length)
 }
 
-function removeDivs()
+function resetDivs()
 {
-    const containerNode = document.getElementById(".container")
-    const childNodes = containerNode.firstChild
-    if(childNodes){
-        containerNode.removeChild(childNodes);
-        childNodes = containerNode.firstChild
-    }
+    var cells = document.querySelectorAll(".gridsquare")
+    let dimension = Math.sqrt(cells.length)
+    genDivs(dimension)
 }
-
-
